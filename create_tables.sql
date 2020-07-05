5a9b34092c6b8a0ec081044a79785f3823a764b2
miatran1109 <59214516+miatran1109@users.noreply.github.com>
2020-07-05 13:09:50 +0700

    Add create tables
    Statements that create necessary tables for database

---
+++ create_tables.sql
@@ -0,0 +1,74 @@
+-- create table contain book's info 
+CREATE TABLE Book_info 
+(  
+    ISBN_code int PRIMARY KEY,  
+    Book_title varchar(100),  
+    Language_use varchar(10),  
+    Binding_id int,  
+    No_copies_actual int,  
+    No_copies_current int,  
+    Genre_id int,  
+    Publication_year int  
+);
+
+-- Create table contain binding detail
+CREATE TABLE Binding_details  
+(  
+    Binding_id int PRIMARY KEY,  
+    Binding_Name varchar(50)  
+);
+
+-- create table contain genre details
+CREATE TABLE Genre_details
+(
+	Genre_id int PRIMARY KEY,
+    Genre_name varchar(45)
+);
+
+-- create table contain authour info
+CREATE TABLE Author_info
+(
+	Author_id int PRIMARY KEY,
+    Author_name varchar(45),
+    Birth_place varchar(45)
+);
+
+-- create table contain borrowers info
+CREATE TABLE Borrower_info
+(
+	Borrower_id int PRIMARY KEY,
+    Borrower_name varchar(45),
+    Borrow_from datetime,
+    Borrow_to datetime,
+    Return_date datetime,
+    Check_by int
+);
+
+-- create table contain staff info
+CREATE TABLE Staff_info
+(
+	Staff_id int PRIMARY KEY,  
+    Staff_name varchar(45),  
+    Password_check varchar(45),  
+    Staff_role varchar(45)
+);
+
+-- create table contain reader info
+Create TABLE Reader_info 
+(  
+    Reader_id int PRIMARY KEY,  
+    Reader_name varchar(45),  
+    Sex varchar(20),  
+    Date_of_birth date,  
+    Borrower_id int,  
+    Address varchar(100),  
+    Contact_number varchar(11)  
+);
+
+-- create table contain detail of shelf where the book stored
+CREATE TABLE Shelf_details
+(
+	Shelf_id int PRIMARY KEY,
+    Floor_no int,
+    Shelf_no int
+);

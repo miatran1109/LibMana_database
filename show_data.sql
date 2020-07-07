5a5b2c7cc347b31019314a3f7becb9f926564383
Dennis Dinh Hung <dennisdinhhung@gmail.com>
2020-07-07 23:50:33 +0700

    update show_data

--- show_data.sql
+++ show_data.sql
@@ -1,3 +1,11 @@
-SELECT Book_title, Language_use, Genre_name, Author_name, Binding_Name, Publication_year, Borrow_from, Borrow_to, Floor_no, Shelf_no
+SELECT Book_title, Language_use, Genre_name, Author_name, Binding_Name, Publication_year, Borrow_from, Borrow_to
 
-FROM Book_info, Binding_details, Genre_details, Author_info, Borrower_info, Shelf_details;
+FROM Book_info
+INNER JOIN Binding_details 
+    ON Book_title.Binding_id = Binding_details.Binding_id
+INNER JOIN Genre_details 
+    ON Book_title.Genre_id = Genre_details.Genre_id
+INNER JOIN Author_info 
+    ON Book_title.Author_id = Author_info.Author_id
+INNER JOIN Borrower_info 
+    ON Book_title.Borrower_id = Borrower_info.Borrower_id;
